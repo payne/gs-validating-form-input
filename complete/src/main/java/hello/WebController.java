4918fa61df43c565efe202a4d6438996fa74307e
Matt Payne <Payne@MattPayne.org>
2019-05-12 17:00:14 -0500

    this works.  Is there a better way?

--- complete/src/main/java/hello/WebController.java
+++ complete/src/main/java/hello/WebController.java
@@ -5,31 +5,25 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Controller
 public class WebController implements WebMvcConfigurer {
+	@GetMapping("/")
+	public String showForm(PersonForm personForm) {
+		return "form";
+	}
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/results").setViewName("results");
-    }
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult, Model model) {
 
-    @GetMapping("/")
-    public String showForm(PersonForm personForm) {
-        return "form";
-    }
-
-    @PostMapping("/")
-    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return "form";
-        }
-
-        return "redirect:/results";
-    }
+		if (bindingResult.hasErrors()) {
+			return "form";
+		}
+		model.addAttribute("personForm", personForm);
+		return "results";
+	}
 }
