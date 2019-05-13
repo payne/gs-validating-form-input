a864a8aed2e96e96442d7eebeba597eb31768479
Dustin Wright <nzeroxpd@gmail.com>
2019-05-13 13:25:15 -0500

    Added <head> to form.html along with a link to main.css to set the backgound color of the html element to orange.  This is not working.  Tried gradle bootRun to rebuild the project (guessing that's not actually doing that).  The guess is the project needs to be rebuilt.  Don't recall how to do that yet.

--- complete/src/main/java/hello/PersonForm.java
+++ complete/src/main/java/hello/PersonForm.java
@@ -14,6 +14,22 @@ public class PersonForm {
     @Min(18)
     private Integer age;
 
+    // add street address and email, years of experience, and a long
+    // add more data labels & fields
+    // append these to the text file
+
+    /*
+    1) Please fork this repo:
+    https://github.com/payne/gs-validating-form-input  
+    Background reading is at https://spring.io/guides/gs/validating-form-input/ 
+    2) run it.   I like: `gradle bootRun`
+    3) Add some fields and validations
+    4) Write a method: savePerson(String fileName, PersonForm person) to append a CSV line to a file -- the data in the line should be from one instance of PersonForm
+    5) call savePerson just before checkPersonInfo return results
+
+    Send me a pull request with your changes.   Thanks! --Matt Payne
+    */
+
     public String getName() {
         return this.name;
     }
